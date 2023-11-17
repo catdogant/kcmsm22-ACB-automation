8338fffd3f4441ccb8acbda71cfedc04b0f18310
Admin <Admin@DESKTOP-S95H1DJ>
2023-11-17 10:53:26 +0530

    commit 2- test class

--- src/main/java/generic_utility/RetrayAnalyzerImplementation.java
+++ src/main/java/generic_utility/RetrayAnalyzerImplementation.java
@@ -1,5 +1,21 @@
 package generic_utility;
 
-public class RetrayAnalyzerImplementation {
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetrayAnalyzerImplementation implements IRetryAnalyzer {
+	int count = 1;
+	int retrycount =4;
+
+	@Override
+	public boolean retry(ITestResult result) {
+		while (count<retrycount) {
+			count ++;
+			return true;
+		}
+		
+		return false;
+	}
+	
 
 }
