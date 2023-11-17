1e8c93f88cd6ed0a3fa8f54d99de5acd73b9bc18
Admin <Admin@DESKTOP-S95H1DJ>
2023-11-17 12:05:46 +0530

    commit push for 2nd time

--- src/test/java/TestCase_Repo/Test.java
+++ src/test/java/TestCase_Repo/Test.java
@@ -2,7 +2,10 @@ package TestCase_Repo;
 
 public class Test {
 	public  static void  main (String args[]) {
+		
 		System.out.println(" push test class to git ");	
+		
+		System.out.println("pull test")	;	
 		}
 
 }
