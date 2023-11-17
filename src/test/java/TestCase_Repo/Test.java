86337ebe1ed11701b55da8056160606f9af8cb33
Admin <Admin@DESKTOP-S95H1DJ>
2023-11-17 11:00:47 +0530

    commit-2 test class to git

--- src/test/java/TestCase_Repo/Test.java
+++ src/test/java/TestCase_Repo/Test.java
@@ -2,7 +2,7 @@ package TestCase_Repo;
 
 public class Test {
 	public  static void  main (String args[]) {
-		System.out.println("test ");	
+		System.out.println(" push test class to git ");	
 		}
 
 }
